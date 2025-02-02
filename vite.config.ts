58c7d221b8bb82f5461451252f67690bb33606a8
SAK74 <sergijks@gmail.com>
2025-02-02 10:35:49 +0100

    build: add tailwindcss & upgrade react/react/dom

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tsConfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
+  plugins: [react(), tsConfigPaths(), tailwindcss()],
   server: {
     host: true,
     port: 3000,
