043ddcf52e95985aff1c89496a096b948aa4374f
SAK74 <sergijks@gmail.com>
2025-02-09 21:47:34 +0100

    feat: add router error boundary

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,26 @@
+import ErrorElement from 'components/ErrorElement';
 import { Details, loader } from 'pages/Details';
 import Home from 'pages/Home';
 import { Navigate, type RouteObject } from 'react-router';
 
 export const routes: RouteObject[] = [
   {
-    index: true,
-    element: <Navigate to={'search'} />,
-  },
-  {
-    path: 'search',
-    element: <Home />,
+    errorElement: <ErrorElement />,
     children: [
       {
-        path: 'details/:detailsID',
-        loader,
-        element: <Details />,
+        index: true,
+        element: <Navigate to={'search'} />,
+      },
+      {
+        path: 'search',
+        element: <Home />,
+        children: [
+          {
+            path: 'details/:detailsID',
+            loader,
+            element: <Details />,
+          },
+        ],
       },
     ],
   },
